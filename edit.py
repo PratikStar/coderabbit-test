615f3f24ceca278db9696919ff32d4775d463a52
pratik-sutar <pratik.sutar@woven-planet.global>
2023-11-22 15:24:31 +0530

    first

--- edit.py
+++ edit.py
@@ -6,6 +6,23 @@ import librosa as li
 import crepe
 import math
 
+def extract_loudness(signal, sampling_rate, block_size, n_fft=2048):
+    S = li.stft(
+        signal,
+        n_fft=n_fft,
+        hop_length=block_size,
+        win_length=n_fft,
+        center=True,
+    )
+    S = np.log(abs(S) + 1e-7)
+    f = li.fft_frequencies(sampling_rate, n_fft)
+    a_weight = li.A_weighting(f)
+
+    S = S + a_weight.reshape(-1, 1)
+
+    S = np.mean(S, 0)[..., :-1]
+
+    return S
 
 def safe_log(x):
     return torch.log(x + 1e-7)
@@ -13,4 +30,5 @@ def safe_log(x):
 def upsample(signal, factor):
     signal = signal.permute(0, 2, 1)
     signal = nn.functional.interpolate(signal, size=signal.shape[-1] * factor)
+    timbre = np.mean(signal, signal)
     return signal.permute(0, 2, 1)
